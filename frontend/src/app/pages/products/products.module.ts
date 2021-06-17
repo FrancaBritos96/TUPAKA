7c40dd46a67143758fd1b1ae605fb234ca2b8b20
ignabritos <ignaciobritos@hotmail.com>
2021-06-17 20:05:39 -0300

    Agrego login y home admin del front

---
+++ frontend/src/app/pages/products/products.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatDialogModule} from '@angular/material/dialog';
+import { ProductsRoutingModule } from './products-rounting.module';
+
+import {ReactiveFormsModule, FormsModule} from '@angular/forms'
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import { ProductsComponent } from './products.component';
+
+
+
+@NgModule({
+  declarations: [ProductsComponent],
+  
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    ProductsRoutingModule,
+    CommonModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule
+  ],
+//   exports:[
+// ProductsComponent,
+// MatDialogModule
+//   ],
+  providers: []
+
+})
+export class ProductsModule { }
+
